1fd4a1bccfbd4dc105047555f4d073ec5c067cd9
Eduardo Dadalto <eduardo@futureframe.ai>
2024-08-12 11:55:37 -0700

    improve cm2 example

--- examples/cm2.py
+++ examples/cm2.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 import futureframe as ff
 
 
-dataset_name = "tests/data/churn.csv"
+dataset_name = "https://raw.githubusercontent.com/futureframeai/futureframe/main/tests/data/churn.csv"
 target_variable = "Churn"
 df = pd.read_csv(dataset_name)
 df.info()
